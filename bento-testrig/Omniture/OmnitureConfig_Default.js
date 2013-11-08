78886956a47842874c66ddb9ce7c6ec123ce2559
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2013-11-08 13:37:11 +0300

    add json conf

---
+++ bento-testrig/Omniture/OmnitureConfig_Default.js
@@ -0,0 +1,15 @@
+[
+    {
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "bento android test channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+                "demdexEnabled" : "true"
+     }
+];
