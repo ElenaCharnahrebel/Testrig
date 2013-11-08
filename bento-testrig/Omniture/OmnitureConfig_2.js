1ff8afc8cbacd03d301a5fa84fb7c6663284847e
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2013-11-08 16:51:27 +0300

    2-new

--- bento-testrig/Omniture/OmnitureConfig_2.js
+++ bento-testrig/Omniture/OmnitureConfig_2.js
@@ -1,14 +1,15 @@
 [
-	{
+    {
 		"omnitureEnabled" : "true",
-		"omnitureSuite"   : "alena",
-		"omnitureChannel" : "Bento TestRig Channel",
+		"omnitureSuite"   : "lalala",
+		"omnitureChannel" : "bento android test channel",
 		"omnitureCharSet" : "ISO-8859-1",
 		"omnitureCurrencyCode" : "USD",
 		"omnitureSSL" : "false",
 		"omnitureLifecycleSessionTimeout" : "300",
 		"omnitureTrackingServer" : "sc.mtv.com",
 		"omnitureVisitorID" : "",
-		"omnitureDebugLogging" : "true" 
-    	}
+		"omnitureDebugLogging" : "true",
+                "demdexEnabled" : "true"
+     }
 ];
