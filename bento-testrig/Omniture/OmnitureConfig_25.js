0114c0256fea7da61b3c26e9ce6319d811d07c16
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2013-11-08 18:09:15 +0300

    case25

---
+++ bento-testrig/Omniture/OmnitureConfig_25.js
@@ -0,0 +1,15 @@
+[
+    {
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "NULL",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+                "demdexEnabled" : "true"
+     }
+];
