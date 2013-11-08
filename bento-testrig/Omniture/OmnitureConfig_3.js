a047327f1fe7635813ddabc7f14f94167afadc80
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2013-11-08 15:13:22 +0300

    omniture

---
+++ bento-testrig/Omniture/OmnitureConfig_3.js
@@ -0,0 +1,15 @@
+[
+    {
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "bento android test channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "lala.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+                "demdexEnabled" : "true"
+     }
+];
